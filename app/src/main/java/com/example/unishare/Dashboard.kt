237bb00bce96e4d57cac9693b97dd3d4cdced7e4
Abhijeet Prasad <abhi.prasad16@gmail.com>
2021-03-07 17:58:00 +0530

    Auth Module Complete (#8)
    * modified UI
    
    * added logo
    
    * Email/Pass Signin | Create User
    
    * Email/Pass Signin | Create User (#1)
    
    Co-authored-by: Abhijeet Prasad <abhi.prasad16@gmail.com>
    
    * Login and Signin | Separate Functionality
    
    * Google SignIn
    
    * GSignin + Logout
    
    Co-authored-by: Silvi <silvi.entrprise17@gmail.com>
    Co-authored-by: Silvi Sharma <47741827+silvisharma@users.noreply.github.com>

--- app/src/main/java/com/example/unishare/Dashboard.kt
+++ app/src/main/java/com/example/unishare/Dashboard.kt
@@ -1,5 +1,6 @@
 package com.example.unishare
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -10,17 +11,19 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-
 class Dashboard : AppCompatActivity() {
 
     private lateinit var logoutButton: Button
+    private fun reload(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-
         val userEmail = findViewById<TextView>(R.id.currentUserEmail)
-
         val user = FirebaseAuth.getInstance().currentUser
         val userid = user!!.uid
         userEmail.setText("Logged in as " + user.email);
@@ -29,6 +32,7 @@ class Dashboard : AppCompatActivity() {
         logoutButton = findViewById<Button>(R.id.logout)
         logoutButton.setOnClickListener(){
             Firebase.auth.signOut()
+            reload()
             }
         }
     }
