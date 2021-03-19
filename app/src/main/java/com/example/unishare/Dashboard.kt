8b5252f03235c5c8e690be81a0fca434b39b44a1
Abhijeet Prasad <abhi.prasad16@gmail.com>
2021-03-19 13:36:42 +0530

    Navigation Drawer Activity (#10)
    * modified UI
    
    * added logo
    
    * Email/Pass Signin | Create User
    
    * Email/Pass Signin | Create User (#1)
    
    Co-authored-by: Abhijeet Prasad <abhi.prasad16@gmail.com>
    
    * Login and Signin | Separate Functionality
    
    * Google SignIn
    
    * GSignin + Logout
    
    * Dashboard with Navigation Activity | Logged in userInfo

--- app/src/main/java/com/example/unishare/Dashboard.kt
+++ app/src/main/java/com/example/unishare/Dashboard.kt
@@ -1,38 +1,72 @@
 package com.example.unishare
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import android.view.Menu
 import android.view.View
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class Dashboard : AppCompatActivity() {
 
-    private lateinit var logoutButton: Button
-    private fun reload(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        val userEmail = findViewById<TextView>(R.id.currentUserEmail)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val navigationView : NavigationView  = findViewById(R.id.nav_view)
+        val headerView : View = navigationView.getHeaderView(0)
+
+        val userEmail = headerView.findViewById<TextView>(R.id.userDisplayEmail)
+        val userName = headerView.findViewById<TextView>(R.id.userDisplayName)
+        val userPhoto = headerView.findViewById<ImageView>(R.id.userDisplayPhoto)
         val user = FirebaseAuth.getInstance().currentUser
         val userid = user!!.uid
-        userEmail.setText("Logged in as " + user.email);
+        userEmail.setText(user.email)
+        userName.setText(user.displayName)
+        userPhoto.setImageURI(user.photoUrl)
 
-        // logout current user
-        logoutButton = findViewById<Button>(R.id.logout)
-        logoutButton.setOnClickListener(){
-            Firebase.auth.signOut()
-            reload()
-            }
-        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.dashboard, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
